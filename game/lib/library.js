67bce3b1b8888b7f25292d8d32e1f0ec1f51f455
Albert <albertlimca@gmail.com>
2015-11-23 17:46:18 +0800

    Use synchronous read file

--- game/lib/library.js
+++ game/lib/library.js
@@ -1,21 +1,12 @@
 var fs = require('fs')
 
-const sowpods = fs.readFileSync(__dirname + '/sowpods.txt', 'utf8').split('\n')
+// sync readFile
+// const sowpods = fs.readFileSync(__dirname + '/sowpods.txt', 'utf8').split('\n')
+
+// async readFile
+const sowpods = fs.readFileSync(__dirname + '/sowpods.txt', 'utf8', (err, library) => {
+  if (err) console.log(err)
+  return library.split('\n')
+})
 
 export default sowpods
-
-// async function checkDictionary (words) {
-//   return await * words.map(word => getDictionary(word))
-// }
-//
-// async function getDictionary (word) {
-//   return await db.get(word, function (error, score) {
-//     if (error) {
-//       if (!error.notFound) {
-//         console.log(error)
-//       }
-//     } else {
-//       return score
-//     }
-//   })
-// }
